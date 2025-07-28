fd22f3038d0c510cf9dab952f47ecf8a1b5ee7bd
spookyexe <gianepanto@gmail.com>
2025-07-28 15:43:00 +0800

    FEAT: dashboard overview cards

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
+import OverviewCard from '@/components/dashboard/overview-card';
+
+import { Bell, Calendar, DollarSign } from 'lucide-react';
 
 export default async function Dashboard() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
       <h1 className="text-xl font-bold sm:text-2xl md:text-3xl">Dashboard</h1>
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-        <div className="bg-muted/50 aspect-video rounded-xl" />
+      <div className="grid grid-cols-2 grid-rows-2 gap-4 lg:grid-cols-3 lg:grid-rows-1">
+        <OverviewCard
+          title="New Announcements"
+          value="12"
+          description="+3 from yesterday"
+          icon={Bell}
+          className="row-span-1"
+        />
+        <OverviewCard
+          title="Upcoming Events"
+          value="8"
+          description="This week"
+          icon={Calendar}
+          className="row-span-1"
+        />
+        <OverviewCard
+          title="Stipend Status"
+          value="Pending"
+          description="Awaiting approval"
+          icon={DollarSign}
+          className="col-span-2 lg:col-span-1"
+        />
       </div>
       <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
     </div>
---
+++ src/components/dashboard/overview-card.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { type LucideIcon } from 'lucide-react';
+import { Card } from '@/components/ui/card';
+
+const OverviewCard = (props: {
+  title: string;
+  value: string;
+  description: string;
+  icon: LucideIcon;
+  className?: string;
+}) => {
+  return (
+    <Card className={`flex !gap-0 p-4 md:p-6 ${props.className}`}>
+      <div className="flex items-center justify-between">
+        <h1 className="text-card-foreground text-xs font-medium md:text-sm">
+          {props.title}
+        </h1>
+        <props.icon className="text-muted-foreground size-3 md:size-5" />
+      </div>
+      <span className="text-foreground text-2xl font-extrabold md:text-4xl">
+        {props.value}
+      </span>
+      <p className="text-muted-foreground text-[10px] md:text-xs">
+        {props.description}
+      </p>
+    </Card>
+  );
+};
+
+export default OverviewCard;
