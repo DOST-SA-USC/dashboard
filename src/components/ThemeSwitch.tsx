9a2416459d86ee1b08a43f00a3b217d005b5ecaa
spookyexe <gianepanto@gmail.com>
2025-07-29 15:14:18 +0800

    STYLE: Change System theme icon

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import * as React from 'react';
-import { Moon, Sun, MonitorCog } from 'lucide-react';
+import { Moon, Sun, SunMoon } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
 import { Button } from '@/components/ui/button';
@@ -26,7 +26,7 @@ export default function ModeToggle(props: {
           ) : theme === 'light' ? (
             <Sun className="h-5 w-5" />
           ) : (
-            <MonitorCog className="h-5 w-5" />
+            <SunMoon className="h-5 w-5" />
           )}
           <span className="sr-only">Toggle theme</span>
         </Button>
