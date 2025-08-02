2a55b2f5716425ef7f1dc27e4bd7d669959c625d
spookyexe <gianepanto@gmail.com>
2025-08-02 14:50:36 +0800

    STYLE: announcemnt top bar

--- src/app/dashboard/announcements/components/AnnouncementContent.tsx
+++ src/app/dashboard/announcements/components/AnnouncementContent.tsx
@@ -1,7 +1,6 @@
 /* 
   - add debounce  once search functionality is implemented
   - add pagination
-
 */
 
 'use client';
@@ -47,52 +46,53 @@ const AnnouncementContent = () => {
 
   return (
     <>
-      <div className="flex w-full gap-4">
-        <div className="flex w-full items-center gap-2">
-          <div className="placeholder:text-muted-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 items-center rounded-md border bg-transparent px-3 py-1 text-sm shadow-xs transition-[color,box-shadow] outline-none">
-            <Search className="text-muted-foreground size-4" />
-            <input
-              placeholder="Search announcements..."
-              type="search"
-              className="placeholder:text-muted-foreground w-full p-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-            />
-          </div>
-        </div>
-        <Select
-          onValueChange={(value) => setActiveFilter(value)}
-          value={activeFilter}
-        >
-          <SelectTrigger className="w-44">
-            <SelectValue placeholder="Filter by" />
-          </SelectTrigger>
-          <SelectContent className="w-44">
-            <SelectGroup>
-              <SelectLabel>Select Filter</SelectLabel>
-              <SelectItem value="all">All</SelectItem>
-              <SelectItem value="officer">Officer</SelectItem>
-              <SelectItem value="faculty">Faculty</SelectItem>
-              <SelectItem value="facebook">Facebook</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </div>
-
-      <div className="border-border flex h-full items-start justify-between border-t">
-        <div className="mt-4 max-h-[70vh] w-full space-y-4 overflow-y-auto p-0 md:w-3/5 md:p-2">
-          {filteredAnnouncements.map((announcement, index) => (
-            <AnnouncementItem
-              key={index}
-              announcement={announcement}
-              onClick={() => setSelectedAnnouncement(announcement)}
-            />
-          ))}
-          {filteredAnnouncements.length === 0 && (
-            <div className="flex h-full w-full items-center justify-center">
-              <p className="text-muted-foreground">No announcements found.</p>
+      <div className="border-border flex h-full max-h-[80vh] items-start justify-between overflow-hidden border-t">
+        <div className="flex h-full w-full flex-col gap-4 md:w-3/5">
+          <div className="border-border flex w-full gap-4 border-b py-3.5 md:p-3.5">
+            <div className="flex w-full items-center gap-2">
+              <div className="placeholder:text-muted-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 items-center rounded-md border bg-transparent px-3 py-1 text-sm shadow-xs transition-[color,box-shadow] outline-none">
+                <Search className="text-muted-foreground size-4" />
+                <input
+                  placeholder="Search announcements..."
+                  type="search"
+                  className="placeholder:text-muted-foreground w-full p-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                />
+              </div>
             </div>
-          )}
+            <Select
+              onValueChange={(value) => setActiveFilter(value)}
+              value={activeFilter}
+            >
+              <SelectTrigger className="w-44">
+                <SelectValue placeholder="Filter by" />
+              </SelectTrigger>
+              <SelectContent className="w-44">
+                <SelectGroup>
+                  <SelectLabel>Select Filter</SelectLabel>
+                  <SelectItem value="all">All</SelectItem>
+                  <SelectItem value="officer">Officer</SelectItem>
+                  <SelectItem value="faculty">Faculty</SelectItem>
+                  <SelectItem value="facebook">Facebook</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="h-full w-full space-y-4 overflow-y-auto px-0 md:px-2">
+            {filteredAnnouncements.map((announcement, index) => (
+              <AnnouncementItem
+                key={index}
+                announcement={announcement}
+                onClick={() => setSelectedAnnouncement(announcement)}
+              />
+            ))}
+            {filteredAnnouncements.length === 0 && (
+              <div className="flex h-full w-full items-center justify-center">
+                <p className="text-muted-foreground">No announcements found.</p>
+              </div>
+            )}
+          </div>
         </div>
 
         <SelectedAnnouncement
--- src/app/dashboard/announcements/components/selected-announcement.tsx
+++ src/app/dashboard/announcements/components/selected-announcement.tsx
@@ -31,17 +31,6 @@ const SelectedAnnouncement = (props: {
     }
   }, [props.announcement, isMobile]);
 
-  if (!props.announcement) {
-    return (
-      <div className="border-border ml-2 hidden h-full w-full flex-col gap-2 border-l p-4 md:flex md:items-center md:justify-center">
-        <MousePointerClick className="text-muted-foreground size-8" />
-        <span className="text-muted-foreground text-sm">
-          No Selected Announcement
-        </span>
-      </div>
-    );
-  }
-
   if (isMobile) {
     return (
       <Drawer
@@ -51,9 +40,44 @@ const SelectedAnnouncement = (props: {
       >
         <DrawerContent>
           <DrawerHeader className="sr-only">
-            <DrawerTitle>{props.announcement.title}</DrawerTitle>
+            <DrawerTitle>{props.announcement?.title}</DrawerTitle>
           </DrawerHeader>
           <div className="border-border mt-4 flex items-center justify-between border-y px-6 py-4">
+            <div>
+              <div className="flex items-center gap-3">
+                <Avatar className="bg-accent flex size-8 items-center justify-center text-base font-medium">
+                  {getUserInitials(props.announcement?.author || '')}
+                </Avatar>
+                <div className="flex flex-col">
+                  <p className="text-base leading-4 font-semibold">
+                    {props.announcement?.author}
+                  </p>
+                  <span className="text-xs">DOST SA USC President</span>
+                </div>
+              </div>
+            </div>
+            <span className="text-muted-foreground text-xs">
+              {formatDate(new Date(props.announcement?.createdAt || ''))}
+            </span>
+          </div>
+          <div className="p-4">
+            <h1 className="text-center text-xl font-extrabold">
+              {props.announcement?.title}
+            </h1>
+            <p className="mt-2 text-justify text-sm">
+              {props.announcement?.content}
+            </p>
+          </div>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+
+  return (
+    <div className="border-border hidden h-full w-full border-l p-0 md:flex md:flex-col">
+      {props.announcement ? (
+        <>
+          <div className="flex items-center justify-between gap-3 border-b px-6 py-4">
             <div>
               <div className="flex items-center gap-3">
                 <Avatar className="bg-accent flex size-8 items-center justify-center text-base font-medium">
@@ -72,42 +96,22 @@ const SelectedAnnouncement = (props: {
             </span>
           </div>
           <div className="p-4">
-            <h1 className="text-center text-xl font-extrabold">
-              {props.announcement?.title}
+            <h1 className="text-2xl font-extrabold">
+              {props.announcement.title}
             </h1>
-            <p className="mt-2 text-justify text-sm">
-              {props.announcement?.content}
+            <p className="text-justify text-base">
+              {props.announcement.content}
             </p>
           </div>
-        </DrawerContent>
-      </Drawer>
-    );
-  }
-
-  return (
-    <div className="border-border ml-2 hidden h-full w-full border-l p-0 md:flex md:flex-col">
-      <div className="flex items-center justify-between gap-3 border-b px-6 py-4">
-        <div>
-          <div className="flex items-center gap-3">
-            <Avatar className="bg-accent flex size-8 items-center justify-center text-base font-medium">
-              {getUserInitials(props.announcement.author)}
-            </Avatar>
-            <div className="flex flex-col">
-              <p className="text-base leading-4 font-semibold">
-                {props.announcement.author}
-              </p>
-              <span className="text-xs">DOST SA USC President</span>
-            </div>
-          </div>
+        </>
+      ) : (
+        <div className="flex h-full w-full flex-col items-center justify-center gap-2">
+          <MousePointerClick className="text-muted-foreground size-8" />
+          <span className="text-muted-foreground text-sm">
+            No Selected Announcement
+          </span>
         </div>
-        <span className="text-muted-foreground text-xs">
-          {formatDate(new Date(props.announcement.createdAt))}
-        </span>
-      </div>
-      <div className="p-4">
-        <h1 className="text-2xl font-extrabold">{props.announcement.title}</h1>
-        <p className="text-justify text-base">{props.announcement.content}</p>
-      </div>
+      )}
     </div>
   );
 };
