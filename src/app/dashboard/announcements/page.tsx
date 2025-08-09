979669f539cc2125f9be54bf61d1df940986ae33
spookyexe <gianepanto@gmail.com>
2025-08-10 02:37:35 +0800

    feat: cache announcements

--- src/app/dashboard/announcements/page.tsx
+++ src/app/dashboard/announcements/page.tsx
@@ -3,8 +3,17 @@ import React from 'react';
 import AnnouncementContent from './content';
 import { getAnnouncements } from '@/lib/db/announcements';
 
+import type { AnnouncementType } from '@/type';
+
 export default async function Announcements() {
-  const { announcements, size } = await getAnnouncements();
+  let cachedData: { announcements: AnnouncementType[]; size: number } | null =
+    null;
+
+  if (!cachedData) {
+    cachedData = await getAnnouncements();
+  }
+
+  const { announcements, size } = cachedData;
 
   return (
     <AnnouncementContent announcementData={announcements} totalPages={size} />
