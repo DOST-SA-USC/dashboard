bfa0b74187e8b6f7407dd2974e49f23a1db77e1f
spookyexe <gianepanto@gmail.com>
2025-08-14 22:33:16 +0800

    style: smaller calendar items

--- src/components/dashboard/calendar/full-calendar/calendar-item.tsx
+++ src/components/dashboard/calendar/full-calendar/calendar-item.tsx
@@ -12,7 +12,7 @@ const CalendarItem = (props: {
   return (
     <div
       className={cn(
-        'min-h-8 w-full cursor-pointer overflow-hidden p-1 text-[10px] font-semibold text-wrap text-white hover:opacity-80 sm:text-xs md:min-h-auto md:text-sm md:text-nowrap dark:font-medium dark:text-black',
+        'h-5 w-full cursor-pointer overflow-hidden p-1 text-[10px] font-semibold text-wrap text-white hover:opacity-80 sm:text-xs md:text-sm md:text-nowrap dark:font-medium dark:text-black',
         getBadgeClass(props.arg.event.extendedProps.type[0]).bg
       )}
       onClick={() => props.onEventClick(props.arg.event.extendedProps.eventId)}
