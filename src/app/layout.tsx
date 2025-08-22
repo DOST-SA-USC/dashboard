fae17e673144185b91eaad890c1c98f199bde193
gian.gg <gianepanto@gmail.com>
2025-08-23 01:53:46 +0800

    chore: vercel analytics, seo & metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,74 @@ import '@/styles/globals.css';
 
 import { Toaster } from '@/components/ui/sonner';
 import { ThemeProvider } from '@/components/ui/theme-provider';
+import { Analytics } from '@vercel/analytics/next';
 
 import type { Metadata } from 'next';
+
 export const metadata: Metadata = {
   title: 'DOST SA USC',
   description: 'Official global tracker for DOST SA USC.',
+  applicationName: 'DOST SA USC',
+  keywords: [
+    'DOST Scholars',
+    'University of San Carlos',
+    'USC',
+    'DOST SA USC',
+    'Scholars Association',
+    'Philippines',
+  ],
+  authors: [{ name: 'DOST SA USC' }],
+  creator: 'DOST SA USC',
+  publisher: 'DOST SA USC',
+
+  // SEO robots
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-snippet': -1,
+      'max-image-preview': 'large',
+      'max-video-preview': -1,
+    },
+  },
+
+  // Open Graph (Facebook, LinkedIn, etc.)
+  openGraph: {
+    type: 'website',
+    locale: 'en_PH',
+    url: 'https://dostsausc.org/',
+    siteName: 'DOST SA USC',
+    title: 'DOST SA USC',
+    description: 'Official global tracker for DOST SA USC.',
+    images: [
+      {
+        url: '/banner.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'DOST SA USC Official Banner',
+      },
+    ],
+  },
+
+  // Icons
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+  },
+
+  // Theme color for browsers
+  themeColor: [
+    {
+      media: '(prefers-color-scheme: light)',
+      color: 'oklch(0.3994 0.1003 271.8184)',
+    },
+    {
+      media: '(prefers-color-scheme: dark)',
+      color: 'oklch(57.954% 0.1119 274.075)',
+    },
+  ],
 };
 
 export default function RootLayout({
@@ -23,6 +86,7 @@ export default function RootLayout({
           </div>
           <Toaster position="bottom-right" richColors />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
